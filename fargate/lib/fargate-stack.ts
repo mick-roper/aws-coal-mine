33938e72ad18b656d51e02a0fcc08cabe6da4728
mick-roper <mick.roper@dazn.com>
2019-07-18 13:10:14 +0100

    added some random port shenanigans

--- fargate/lib/fargate-stack.ts
+++ fargate/lib/fargate-stack.ts
@@ -3,6 +3,13 @@ import ec2 = require('@aws-cdk/aws-ec2')
 import ecs = require('@aws-cdk/aws-ecs')
 import ecs_patterns = require('@aws-cdk/aws-ecs-patterns')
 
+const getRandomPort = () => {
+  const min = 30000
+  const max = 65000
+
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
 export class FargateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -11,13 +18,19 @@ export class FargateStack extends cdk.Stack {
 
     const cluster = new ecs.Cluster(this, "fargate-cluster", { vpc })
 
-    new ecs_patterns.LoadBalancedFargateService(this, "some-fargate-service", {
+    const containerPort = getRandomPort()
+
+    const service = new ecs_patterns.LoadBalancedFargateService(this, "some-fargate-service", {
       cluster,
       cpu: 512,
       desiredCount: 1,
       image: ecs.ContainerImage.fromRegistry("chaosd/control-plane"),
-      memoryLimitMiB: 1024,
-      publicLoadBalancer: true
+      memoryLimitMiB: 512,
+      publicLoadBalancer: true,
+      containerPort,
+      environment: {
+        PORT: `${containerPort}`
+      }
     })
   }
 }
