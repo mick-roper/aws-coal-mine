97b8ce30c3ef6416938e0d2897ef6385810da189
mick-roper <mick.roper@dazn.com>
2019-07-18 11:55:49 +0100

    fiddled it all a bit more

--- fargate/bin/fargate.ts
+++ fargate/bin/fargate.ts
@@ -3,5 +3,12 @@ import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { FargateStack } from '../lib/fargate-stack';
 
+const props: cdk.StackProps = {
+  env: {
+    region: 'eu-west-1',
+    account: '317464599277' // symmetric
+  }
+} 
+
 const app = new cdk.App();
-new FargateStack(app, 'FargateStack');
+new FargateStack(app, 'FargateStack', props);
