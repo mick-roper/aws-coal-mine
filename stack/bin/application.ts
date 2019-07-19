e5676b02cd52987073c7b70934577b16ca1a0b8e
mick-roper <mick.roper@dazn.com>
2019-07-19 08:54:04 +0100

    added deps

--- stack/bin/application.ts
+++ stack/bin/application.ts
@@ -5,8 +5,7 @@ import { NetworkStack } from '../lib/network-stack';
 import { EcsClusterStack } from '../lib/ecs-cluster-stack'
 import { ChaosdServiceStack } from '../lib/fargate-service'
 
-const props: cdk.StackProps = { 
-  stackName: 'chaosd-control-plane', 
+const props: cdk.StackProps = {
   env: {
     account: '',
     region: 'eu-west-1'
@@ -17,6 +16,9 @@ const props: cdk.StackProps = {
 }
 
 const app = new cdk.App();
-const networkStack = new NetworkStack(app, 'NetworkStack', props);
+const networkStack = new NetworkStack(app, 'chaosd-network', props);
 const clusterStack = new EcsClusterStack(app, 'chaosd-cluster', { ...props, vpc: networkStack.vpc })
-const chaosdServiceStackv1 = new ChaosdServiceStack(app, 'ChaosdService-v1', { ...props, cluster: clusterStack.cluster })
+const chaosdServiceStackv1 = new ChaosdServiceStack(app, 'chaosd-service-v1', { ...props, cluster: clusterStack.cluster })
+
+chaosdServiceStackv1.addDependency(clusterStack, 'the ecs cluster the app runs on')
+clusterStack.addDependency(networkStack, 'the network the cluster runs on')
