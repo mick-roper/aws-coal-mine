15560fe212001c70a4d1c6415c2acab33d396127
mick-roper <mick.roper@dazn.com>
2019-07-19 08:10:31 +0100

    added a network stack

---
+++ network/bin/network.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import cdk = require('@aws-cdk/core');
+import { NetworkStack } from '../lib/network-stack';
+
+const app = new cdk.App();
+new NetworkStack(app, 'NetworkStack');
---
+++ network/lib/network-stack.ts
@@ -0,0 +1,37 @@
+import cdk = require('@aws-cdk/core');
+import ec2 = require('@aws-cdk/aws-ec2')
+import ecs = require('@aws-cdk/aws-ecs')
+import { Secret } from '@aws-cdk/aws-secretsmanager';
+
+export class NetworkStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'application vpc', {
+      cidr: '10.0.0.0/16',
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          cidrMask: 24, 
+          name: 'public', 
+          subnetType: ec2.SubnetType.PUBLIC
+        },
+        {
+          cidrMask: 24, 
+          name: 'private', 
+          subnetType: ec2.SubnetType.PRIVATE
+        },
+        {
+          cidrMask: 24, 
+          name: 'isolated', 
+          subnetType: ec2.SubnetType.ISOLATED
+        },
+      ],
+      natGatewaySubnets: { 
+        subnetName: 'public' 
+      },
+    })
+
+    new ecs.Cluster(this, 'fargate', { vpc })
+  }
+}
