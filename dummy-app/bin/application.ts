43dd0836da3744aa1905593f9b16698fdf2a1f83
mick-roper <mick.roper@dazn.com>
2019-07-22 10:58:06 +0100

    hiding one of the stacks

--- dummy-app/bin/application.ts
+++ dummy-app/bin/application.ts
@@ -23,14 +23,13 @@ const chaosdServiceStackv0_1_34 = new ChaosdServiceStack(app,
     cluster: sharedStack.cluster, 
     image: 'chaosd/control-plane:0.1.34'
   })
-
-const chaosdServiceStackv0_1_35 = new ChaosdServiceStack(app, 
-  'chaosd-service-0-1-35', 
-  { 
-    ...props, 
-    cluster: sharedStack.cluster, 
-    image: 'chaosd/control-plane:0.1.35'
-  })
-
 chaosdServiceStackv0_1_34.addDependency(sharedStack, 'the root stack')
-chaosdServiceStackv0_1_35.addDependency(sharedStack, 'the root stack')
+
+// const chaosdServiceStackv0_1_35 = new ChaosdServiceStack(app, 
+//   'chaosd-service-0-1-35', 
+//   { 
+//     ...props, 
+//     cluster: sharedStack.cluster, 
+//     image: 'chaosd/control-plane:0.1.35'
+//   })
+// chaosdServiceStackv0_1_35.addDependency(sharedStack, 'the root stack')
