c8a73bdefeec203790b61f0d810005455cb46a2a
mick-roper <mick.roper@dazn.com>
2019-07-22 09:37:57 +0100

    added some props

--- canary/bin/canary.ts
+++ canary/bin/canary.ts
@@ -3,5 +3,14 @@ import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { CanaryStack } from '../lib/canary-stack';
 
+const props: cdk.StackProps = {
+  env: {
+    region: 'eu-west-1'
+  },
+  tags: {
+    project: 'canary-release'
+  }
+}
+
 const app = new cdk.App();
-new CanaryStack(app, 'CanaryStack');
+new CanaryStack(app, 'CanaryStack', props);
