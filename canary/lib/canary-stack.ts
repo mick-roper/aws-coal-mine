45ea93598bc33a8f0dfd08dea09889aa4cb904c2
mick-roper <mick.roper@dazn.com>
2019-07-22 09:34:01 +0100

    added topic

--- canary/lib/canary-stack.ts
+++ canary/lib/canary-stack.ts
@@ -1,9 +1,23 @@
-import cdk = require('@aws-cdk/core');
+import * as cdk from '@aws-cdk/core'
+import * as lambda from '@aws-cdk/aws-lambda'
+import { Topic } from '@aws-cdk/aws-sns'
+import { SnsEventSource } from '@aws-cdk/aws-lambda-event-sources'
 
 export class CanaryStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const topic = new Topic(this, 'canary-trigger', {
+      topicName: 'canary-trigger',
+      displayName: 'Canary Deployment Trigger'
+    })
+
+    const fn = new lambda.Function(this, 'canary-function', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 'index.entryPoint',
+      code: lambda.Code.asset('../canary-fn')
+    })
+
+    fn.addEventSource(new SnsEventSource(topic))    
   }
 }
